c247ceb8fd905e30c40fbcba17035dbb4eece6d8
MarcinSerwinski <MarcinSerwinski@github.com>
2023-02-20 14:57:44 +0100

    extended validat format by .jpg

--- images/validators.py
+++ images/validators.py
@@ -2,6 +2,6 @@ from rest_framework.exceptions import ValidationError
 
 
 def validate_images_extension(upload):
-    valid_formats = ['png', 'jpeg']
+    valid_formats = ['png', 'jpeg', 'jpg']
     if not any([True if upload.name.endswith(i) else False for i in valid_formats]):
-        raise ValidationError(f'{upload.name} is not a valid image format. Only .png and .jpeg are accepted.')
+        raise ValidationError(f'{upload.name} is not a valid image format. Only .png, .jpeg and .jpg are accepted.')
